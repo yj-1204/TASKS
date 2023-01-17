dba85514bcfcb59a9cb5d18bb645bbdd0f422ad0
yj-1204 <119739986+yj-1204@users.noreply.github.com>
2023-01-17 23:25:49 +0530

    Delete Quiz-dvm/quizzer directory

--- Quiz-dvm/quizzer/quiza/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Role(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=3, choices=[('QM', 'Quiz Master'), ('QT', 'Quiz Taker')])
